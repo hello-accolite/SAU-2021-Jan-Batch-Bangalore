f649274d77ceb4607a192aaf9123eb846021e275
aditya41 <aditya2041999@gmail.com>
2021-01-12 20:46:53 +0530

    js1 assignment done

---
+++ javascript2_afternoon/app.js
@@ -0,0 +1,22 @@
+class input {
+    constructor(param) {
+        this.input = param;
+    }
+    calculateLength() {
+        return this.input.length;
+    };
+    getUpperCase() {
+        return this.input.toUpperCase();
+    };
+    getLowerCase() {
+        return this.input.toLowerCase();
+    };
+}
+
+function callFunction() {
+    var inputValue = document.getElementById("inputTxt").value;
+    let inputObject = new input(inputValue);
+    document.getElementById("lengthOp").innerHTML = inputObject.calculateLength();
+    document.getElementById("ucOp").innerHTML = inputObject.getUpperCase();
+    document.getElementById("lcOp").innerHTML = inputObject.getLowerCase();
+}
